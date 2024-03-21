45da5ea04d1d407783ca5d8fb50ec7dd16fd5f26
Chamodh Perera <chamodhperera@outlook.com>
2024-03-21 23:05:06 +0530

    Add Welcome Screen

--- lib/screens/welcome/welcome_screen.dart
+++ lib/screens/welcome/welcome_screen.dart
@@ -1,10 +1,89 @@
 import 'package:flutter/material.dart';
+import 'package:lyft_mate/constants/colors.dart';
+// import 'package:lyft_mate/screens/login_screen.dart';
 
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    var height = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          padding: const EdgeInsets.all(30.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Hero(
+                tag: "logo",
+                child: Image.asset(
+                  "assets/images/carpool-image-4.jpg",
+                  height: height * 0.4,
+                ),
+              ),
+              Column(
+                children: [
+                  Text(
+                    "LyftMate🚀",
+                    style: Theme.of(context).textTheme.displaySmall,
+                  ),
+                  Text(
+                    "join the community, and enjoy the ride",
+                    style: Theme.of(context).textTheme.titleSmall,
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: OutlinedButton(
+                      onPressed: () {
+                        // Navigator.push(
+                        //   context,
+                        //   MaterialPageRoute(
+                        //     builder: (context) => LoginScreen(),),
+                        // );
+                      },
+
+                      style: OutlinedButton.styleFrom(
+                        shape: const RoundedRectangleBorder(),
+                        foregroundColor: kSecondaryColor,
+                        side: const BorderSide(color: kSecondaryColor),
+                        padding: const EdgeInsets.symmetric(vertical: 15.0),
+                        // elevation: 1
+                      ),
+                      child: Text(
+                        "Login".toUpperCase(),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 15.0,
+                  ),
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        shape: const RoundedRectangleBorder(),
+                        foregroundColor: kWhiteColor,
+                        backgroundColor: Colors.green,
+                        side: const BorderSide(color: kSecondaryColor),
+                        padding: const EdgeInsets.symmetric(vertical: 15.0),
+                      ),
+                      child: Text(
+                        "Signup".toUpperCase(),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
