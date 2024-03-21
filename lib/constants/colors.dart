f1a517e68809f2b2d06892eb36f41bb13c68583e
Chamodh Perera <chamodhperera@outlook.com>
2024-03-21 19:21:28 +0530

    Add Welcome Screen

---
+++ lib/constants/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xFFFFE400);
+const kSecondaryColor = Color(0xFF272727);
+const kAccentColor = Color(0xFF001BFF);
+
+const kWhiteColor = Colors.white;
+const kDarkColor = Color(0xFF000000);
+const kCardBgColor = Color(0xFFF7F6F1);
+
