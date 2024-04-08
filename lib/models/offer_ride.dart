d5d5419420bf168f78d97ba44b26b0a4188f9e41
Chamodh Perera <chamodhperera@outlook.com>
2024-04-08 12:18:45 +0530

    Modify Offer Ride Model

--- lib/models/offer_ride.dart
+++ lib/models/offer_ride.dart
@@ -16,7 +16,7 @@ class OfferRide {
   LatLng? dropoffLocation; // LatLng object representing dropoff location
   String? seats;
   String? vehicle;
-  double? rideDistance;
+  String? rideDistance;
   String? pickupCityName;
   String? pickupLocationName;
   String? dropoffCityName;
@@ -25,9 +25,11 @@ class OfferRide {
   String? luggageAllowance;
   String? paymentMode;
   String? rideApproval;
+  String? rideNotes;
   DateTime? date; // Date of the ride
   TimeOfDay? time; // Time of the ride
   double? pricePerSeat; // Price per seat for the ride
+  List<String> ridePreferences = [];
   List<String> passengers = []; // List of passenger user IDs
   List<LatLng> polylinePoints = []; // List to store polyline points
 
@@ -45,16 +47,29 @@ class OfferRide {
     dropoffCityName = null;
     dropoffLocationName = null;
     rideDuration = null;
+    rideNotes = null;
     date = null;
     time = null;
     pricePerSeat = null;
     passengers.clear();
+    ridePreferences.clear();
     polylinePoints.clear();
   }
 
-  // void updatePickupCoordinates(double newLat, double newLng) {
-  //   pickupLocation = LatLng(newLat, newLng);
-  // }
+  void updateRideDetails(String distance, String duration, List<LatLng> polylinePoints) {
+    rideDistance = distance;
+    rideDuration = duration;
+    this.polylinePoints = polylinePoints;
+
+    print("this is the pickup city: $pickupLocationName");
+    print("this is the drop city: $dropoffLocation");
+
+    print("this is ride Distance: $rideDistance");
+    print("this is ride Duration: $rideDuration");
+    print("this is ploylinesss: $polylinePoints");
+
+    // this.polylinePoints = polylinePoints;
+  }
 
   // Update method to set pickup location details
   void setPickupLocation(double lat, double lng, String locationName, String cityName) {
@@ -70,9 +85,6 @@ class OfferRide {
     dropoffCityName = cityName;
   }
 
-  void updateDropoffCoordinates(double newLat, double newLng) {
-    dropoffLocation = LatLng(newLat, newLng);
-  }
 
   void resetPolylinePoints() {
     polylinePoints.clear();
@@ -101,9 +113,36 @@ class OfferRide {
     print("seatssss: $seats");
   }
 
+  void setPreferences(List<String> preferences) {
+    ridePreferences.clear();
+    ridePreferences.addAll(preferences);
+    print("this is preferences $ridePreferences");
+  }
+
+  void setNotes(String notes) {
+    rideNotes = notes;
+    print("this isss notesss: $rideNotes");
+  }
+
   // Setter method to update price per seat
   void setPricePerSeat(double newPrice) {
     pricePerSeat = newPrice;
+    print("PRiceeeeee per seaaat: $pricePerSeat");
+  }
+
+  void setLuggageAllowance(String luggageOption) {
+    luggageAllowance = luggageOption;
+    print("Luggage allowance: $luggageAllowance");
+  }
+
+  void setPaymentMode(String paymentOption) {
+    paymentMode = paymentOption;
+    print("Payment option: $paymentMode");
+  }
+
+  void setRideApproval(String approvalOption) {
+    rideApproval = approvalOption;
+    print("rideee approavaal: $rideApproval");
   }
 
   // Method to add a passenger to the ride
